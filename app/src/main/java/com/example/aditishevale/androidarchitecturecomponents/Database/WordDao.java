fe0bb418c411e2b732e5e5d52933586de391b4ec
aditishevale <aditi.shevale999@gmail.com>
2018-07-28 15:06:57 +0530

    Dao interface Added

---
+++ app/src/main/java/com/example/aditishevale/androidarchitecturecomponents/Database/WordDao.java
@@ -0,0 +1,31 @@
+package com.example.aditishevale.androidarchitecturecomponents.Database;
+
+
+import android.arch.lifecycle.LiveData;
+import android.arch.persistence.room.Delete;
+import android.arch.persistence.room.Insert;
+import android.arch.persistence.room.Query;
+import android.arch.persistence.room.Update;
+
+import java.util.List;
+
+public interface WordDao {
+
+    @Insert
+    void insert(Word word);
+
+    @Update
+    void update(Word word);
+
+
+    @Query("DELETE FROM word_table WHERE id=:id")
+    void delete(int id);
+
+    @Delete
+    void deleteFromObj(Word word);
+
+
+    @Query("SELECT * from word_table ORDER BY word ASC")
+    LiveData<List<Word>> getAllWords();
+
+}
