cd75ea1569ea08d55724612b7b1ab0f288c4862c
aditishevale <aditi.shevale999@gmail.com>
2018-07-28 12:59:40 +0530

    ButterKnife Added

--- app/src/main/java/com/example/aditishevale/androidarchitecturecomponents/MainActivity.java
+++ app/src/main/java/com/example/aditishevale/androidarchitecturecomponents/MainActivity.java
@@ -1,13 +1,29 @@
 package com.example.aditishevale.androidarchitecturecomponents;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.RecyclerView;
+import android.widget.Button;
+import android.widget.EditText;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
 
 public class MainActivity extends AppCompatActivity {
+
+    @BindView(R.id.recyclerView)
+    RecyclerView recyclerView;
+    @BindView(R.id.edt_word)
+    EditText edtWord;
+    @BindView(R.id.btn_add)
+    Button btnAdd;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        ButterKnife.bind(this);
+
+
     }
 }
